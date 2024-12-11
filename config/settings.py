11d5613a3a420167c66ec57ac193f411fdd4eb93
D4egon <85824672+d4egon@users.noreply.github.com>
2024-12-11 10:31:14 +0100

    Update settings.py
    adding auroraDB instead of localhost

--- config/settings.py
+++ config/settings.py
@@ -6,8 +6,8 @@ SECRET_KEY = "FungiFungi"
 
 # Database Configuration
 SQLITE_DB_PATH = "data/maia_emotion_db.db"
-NEO4J_URI = "bolt://localhost:7687"
-NEO4J_USERNAME = "neo4j"
+NEO4J_URI = "neo4j+s://46dc0ffd.databases.neo4j.io:7687"
+NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "4KjyYMe6ovQnU9QaRDnwP1q85Ok_rtB9l5p2yiWLgh8"
 
 # Model Paths
