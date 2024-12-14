3f2c978a7849b27c659562931f83c7a4918909b6
d4egon <hansahj@gmail.com>
2024-12-14 02:39:42 +0100

    update

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,5 @@
-# settings.py
-
-# Flask Configuration
-DEBUG = True
-SECRET_KEY = "FungiFungi"
-
-# Database Configuration
-SQLITE_DB_PATH = "data/maia_emotion_db.db"
-NEO4J_URI = "neo4j+s://46dc0ffd.databases.neo4j.io:7687"
-NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "4KjyYMe6ovQnU9QaRDnwP1q85Ok_rtB9l5p2yiWLgh8"
-
-# Model Paths
-MODEL_PATH = "backend/emotion_recognition_model.h5"
+CONFIG = {
+    "NEO4J_URI": "neo4j+s://46dc0ffd.databases.neo4j.io:7687",
+    "NEO4J_USER": "neo4j",
+    "NEO4J_PASS": "4KjyYMe6ovQnU9QaRDnwP1q85Ok_rtB9l5p2yiWLgh8"
+}
